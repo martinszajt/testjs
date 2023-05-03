71f037437c89b07600c25ce284f657672932e050
Martin Szajt <58597855+martinszajt@users.noreply.github.com>
2023-05-03 15:37:49 -0300

    Update wistia.js

--- wistia.js
+++ wistia.js
@@ -1,8 +1,3 @@
-<script src="//fast.wistia.com/assets/external/E-v1.js" async></script>
-<div class="wistia_embed wistia_async_abcde12345" style="width:640px;height:360px;"></div>
-<script>
 window._wq = window._wq || [];
 _wq.push({ id: 'abcde12345', onReady: function(video) {
   console.log("I got a handle to the video!", video);
-}});
-</script>
